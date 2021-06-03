d3579ddf626e1ee549d637f5c0acafbcdd0ff3a5
EenMacFullstack <Iain.mcdulling@gmail.com>
2021-06-03 15:41:23 +0100

    Restful Routes done and dusted

--- src/main/java/com/CodeClan/BookingSystem/BookingSystem/controllers/CourseController.java
+++ src/main/java/com/CodeClan/BookingSystem/BookingSystem/controllers/CourseController.java
@@ -1,4 +1,28 @@
 package com.CodeClan.BookingSystem.BookingSystem.controllers;
 
+import com.CodeClan.BookingSystem.BookingSystem.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value = "/courses")
+    public ResponseEntity getAllCourses(
+            @RequestParam(name = "id", required = false) Long id,
+            @RequestParam(name = "location", required = false) String location,
+            @RequestParam(name = "rating", required = false) Integer rating
+    ){
+        if (id != null){
+            return new ResponseEntity(courseRepository.getByBookingsCustomerId(id), HttpStatus.OK);
+        }
+        return new ResponseEntity(courseRepository.findAll(),HttpStatus.OK);
+    }
 }
