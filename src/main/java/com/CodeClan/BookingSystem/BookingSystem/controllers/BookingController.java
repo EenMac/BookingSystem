303d9b5a11198a91a4b57a79f7cd69d103f78aa5
EenMacFullstack <Iain.mcdulling@gmail.com>
2021-06-03 15:24:26 +0100

    BookingController done and dusted

--- src/main/java/com/CodeClan/BookingSystem/BookingSystem/controllers/BookingController.java
+++ src/main/java/com/CodeClan/BookingSystem/BookingSystem/controllers/BookingController.java
@@ -2,7 +2,13 @@ package com.CodeClan.BookingSystem.BookingSystem.controllers;
 
 import com.CodeClan.BookingSystem.BookingSystem.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.GeneratedValue;
 
 @RestController
 public class BookingController {
@@ -10,5 +16,14 @@ public class BookingController {
     @Autowired
     BookingRepository bookingRepository;
 
+    @GetMapping(value = "/bookings")
+    public ResponseEntity getBookings(
+            @RequestParam(name = "date", required = false) String date
+    ){
+        if(date != null){
+            return new ResponseEntity(bookingRepository.getByBookingDate(date), HttpStatus.OK);
+        }
+        return new ResponseEntity(bookingRepository.findAll(), HttpStatus.OK);
+    }
 
 }
